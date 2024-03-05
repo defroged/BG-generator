27b607841b5e29c74b709ae7c46d61d7f0459160
Ron Ward <defroged@gmail.com>
2024-03-05 13:42:31 +0900

    fix menu

--- main.js
+++ main.js
@@ -2,26 +2,27 @@ function toggleNav() {
     var sideNav = document.getElementById("sideNav");
     var mainContent = document.querySelector(".main-content");
     var hamburgerIcon = document.querySelector(".hamburger-icon");
-    var eslLink = document.querySelector(".esl-link"); // Get the reference to the site logo
+    var eslLink = document.querySelector(".esl-link");
+    var submitGameLink = document.getElementById("submitGameLink");
     var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
 
     if (sideNav.style.width === "250px") {
         sideNav.style.width = "0";
-        // Only adjust marginLeft if not on mobile
         if (viewportWidth > 768) {
             mainContent.style.marginLeft = "0";
+            submitGameLink.style.left = "0"; 
         }
         hamburgerIcon.style.left = "15px";
         hamburgerIcon.style.top = "15px";
-        eslLink.style.left = "50px"; // Move the site logo back
+        eslLink.style.left = "50px";
     } else {
         sideNav.style.width = "250px";
-        // Only adjust marginLeft if not on mobile
         if (viewportWidth > 768) {
             mainContent.style.marginLeft = "250px";
+            submitGameLink.style.left = "250px"; 
         }
         hamburgerIcon.style.left = "200px";
         hamburgerIcon.style.top = "15px";
-        eslLink.style.left = "260px"; // Move the site logo to the right
+        eslLink.style.left = "260px";
     }
 }
