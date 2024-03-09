69c92e4f65f2f8ad3adda6d7f203fbd19a720823
Ron Ward <defroged@gmail.com>
2024-03-09 16:49:09 +0900

    better positioning

--- snakesMapper.js
+++ snakesMapper.js
@@ -3,30 +3,33 @@ const { rgb, StandardFonts } = require('pdf-lib');
 async function addTextToPdf(pdfDoc, fields) {
   const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
 
-  const pages = pdfDoc.getPages()
+  const pages = pdfDoc.getPages();
   const firstPage = pages[0];
   const { width, height } = firstPage.getSize();
 
-  // Get all input fields starting with "box"
-  const boxKeys = Object.keys(fields).filter(key => key.startsWith('box'));
-
-  // Calculate x and y positions
-  const startY = 20;
-  const lineHeight = 20;
-  const offsetX = 160;
+  const positions = [
+    { x: 160, y: 20 },
+    { x: 250, y: 20 },
+    { x: 160, y: 40 },
+    { x: 250, y: 40 },
+    // Add more custom positions if necessary
+  ];
 
   boxKeys.forEach((boxKey, index) => {
     const inputTextArray = fields[boxKey];
     const inputText = Array.isArray(inputTextArray) && inputTextArray.length > 0 ? inputTextArray[0] : '';
 
+    // Get the corresponding position from the positions array
+    const position = positions[index];
+
     firstPage.drawText(inputText, {
-      x: offsetX,
-      y: startY + index * lineHeight,
+      x: position.x,
+      y: position.y,
       size: 16,
       font: helveticaFont,
       color: rgb(0.95, 0.1, 0.1),
     });
-});
+  });
 }
 
 module.exports = {
