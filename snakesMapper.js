0c278579f53df6d77a8b9f1f9cb7b8eddf638d0c
Ron Ward <defroged@gmail.com>
2024-03-09 17:49:48 +0900

    sizing text and custom font

--- snakesMapper.js
+++ snakesMapper.js
@@ -1,16 +1,40 @@
-const { rgb, StandardFonts } = require('pdf-lib');
+const { PDFDocument, rgb } = require('pdf-lib');
+const fontkit = require('@pdf-lib/fontkit');
 
-async function addTextToPdf(pdfDoc, fields) {
-  const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+// A function to wrap text to fit within a certain width
+function wrapText(font, text, maxWidth, fontSize) {
+  const words = text.split(' ');
+  const lines = [];
+  let currentLine = words[0];
 
-  const pages = pdfDoc.getPages()
+  for (let i = 1; i < words.length; i++) {
+    const word = words[i];
+    const width = font.widthOfTextAtSize(`${currentLine} ${word}`, fontSize);
+    if (width < maxWidth) {
+      currentLine += ` ${word}`;
+    } else {
+      lines.push(currentLine);
+      currentLine = word;
+    }
+  }
+  lines.push(currentLine); // Add the last line
+  return lines;
+}
+
+async function addTextToPdf(pdfBytes, fields) {
+  const pdfDoc = await PDFDocument.load(pdfBytes);
+  pdfDoc.registerFontkit(fontkit);
+
+  // Use the URL to your font or replace with a path to a font file if running locally
+  const fontBytes = await fetch('https://pdf-lib.js.org/assets/ubuntu/Ubuntu-R.ttf')
+    .then((res) => res.arrayBuffer());
+  const customFont = await pdfDoc.embedFont(fontBytes);
+
+  const pages = pdfDoc.getPages();
   const firstPage = pages[0];
-  const { width, height } = firstPage.getSize();
+  const boxWidth = 70; // Define the width of the boxes
+  const fontSize = 16; // Starting font size
 
-  // Get all input fields starting with "box"
-  const boxKeys = Object.keys(fields).filter(key => key.startsWith('box'));
-
-  // Define an array of positions for each input field (x, y)
   const positions = [
     { x: 160, y: 20 },
     { x: 230, y: 20 },
@@ -32,23 +56,26 @@ async function addTextToPdf(pdfDoc, fields) {
     { x: 650, y: 90 },
 ];
 
-  boxKeys.forEach((boxKey, index) => {
-    const inputTextArray = fields[boxKey];
-    const inputText = Array.isArray(inputTextArray) && inputTextArray.length > 0 ? inputTextArray[0] : '';
-
-    // Get the corresponding position from the positions array
+  Object.keys(fields).filter(key => key.startsWith('box')).forEach((boxKey, index) => {
+    const inputText = fields[boxKey];
     const position = positions[index];
+    const lines = wrapText(customFont, inputText, boxWidth, fontSize);
 
-    firstPage.drawText(inputText, {
-      x: position.x,
-      y: position.y,
-      size: 16,
-      font: helveticaFont,
-      color: rgb(0.95, 0.1, 0.1),
+    lines.forEach((line, lineIndex) => {
+      firstPage.drawText(line, {
+        x: position.x,
+        y: position.y - (lineIndex * (fontSize + 5)), // Adjust Y based on the line index
+        size: fontSize,
+        font: customFont,
+        color: rgb(0.95, 0.1, 0.1),
+      });
     });
   });
+
+  return pdfDoc.save();
 }
 
+// Export the function so it can be used in other files
 module.exports = {
   addTextToPdf
 };
