d45e07eb4cd1cb480065cf47758ea8cf2d4dfe2f
Ron Ward <defroged@gmail.com>
2024-03-10 17:53:55 +0900

    auto fit in box

--- snakesMapper.js
+++ snakesMapper.js
@@ -1,6 +1,47 @@
 const fontkit = require('@pdf-lib/fontkit');
 const { rgb, StandardFonts } = require('pdf-lib');
 
+function fitTextToBox(text, font, defaultFontSize, maxWidth, maxHeight) {
+  let lines = [];
+  let fontSize = defaultFontSize;
+  let words, currentLineWords, currentLineWidth, potentialLineWidth;
+
+  while (fontSize > 1) {
+    lines = [];
+    words = text.split(' ');
+
+    while (words.length > 0) {
+      currentLineWords = [];
+      currentLineWidth = 0;
+
+      while (words.length > 0) {
+        potentialLineWidth = font.widthOfTextAtSize(currentLineWords.concat(words[0]).join(' '), fontSize);
+
+        if (potentialLineWidth <= maxWidth) {
+          currentLineWords.push(words.shift());
+          currentLineWidth = potentialLineWidth;
+        } else {
+          break;
+        }
+      }
+
+      lines.push(currentLineWords.join(' '));
+
+      if ((lines.length + 1) * font.heightAtSize(fontSize) > maxHeight) {
+        break;
+      }
+    }
+
+    if (words.length === 0) {
+      return { fontSize, lines };
+    }
+
+    fontSize--;
+  }
+
+  return { fontSize, lines: [text] };
+}
+
 async function addTextToPdf(pdfDoc, fields) {
   const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
 
@@ -8,10 +49,8 @@ async function addTextToPdf(pdfDoc, fields) {
   const firstPage = pages[0];
   const { width, height } = firstPage.getSize();
 
-  // Get all input fields starting with "box"
   const boxKeys = Object.keys(fields).filter(key => key.startsWith('box'));
 
-  // Define an array of positions for each input field (x, y)
   const positions = [
     { x: 160, y: 20 },
     { x: 230, y: 20 },
@@ -36,17 +75,28 @@ async function addTextToPdf(pdfDoc, fields) {
   boxKeys.forEach((boxKey, index) => {
     const inputTextArray = fields[boxKey];
     const inputText = Array.isArray(inputTextArray) && inputTextArray.length > 0 ? inputTextArray[0] : '';
-
-    // Get the corresponding position from the positions array
     const position = positions[index];
 
-    firstPage.drawText(inputText, {
-      x: position.x,
-      y: position.y,
-      size: 16,
-      font: helveticaFont,
-      color: rgb(0.95, 0.1, 0.1),
-    });
+    // Set maxWidth and maxHeight for a 70x70 point box
+const maxWidth = 70;
+const maxHeight = 70;
+
+const { fontSize, lines } = fitTextToBox(inputText, helveticaFont, 16, maxWidth, maxHeight);
+const lineSpacing = 1.2;
+const lineHeight = helveticaFont.heightAtSize(fontSize);
+
+let lineY = position.y;
+
+lines.forEach((line) => {
+  firstPage.drawText(line, {
+    x: position.x,
+    y: lineY,
+    size: fontSize,
+    font: helveticaFont,
+    color: rgb(0.95, 0.1, 0.1),
+  });
+  lineY -= lineHeight * lineSpacing;
+});
   });
 }
 
